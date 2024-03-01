ba54be88c42ed7ff2ad04c9e4bdfd702701cfd67
Damon Alexander <51380857+DaemonAlex@users.noreply.github.com>
2024-03-01 16:07:31 -0600

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,10 @@
+fx_version 'cerulean'
+games { 'gta5' }
+
+author 'DaemonAlex "Schtoop"'
+description 'Access control for FivePD functionalities based on QBCore police job.'
+version '1.0.0'
+
+server_script 'server.lua'
+
+dependency 'qb-core'
