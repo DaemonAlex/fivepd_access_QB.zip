1e083d0d60508dac476a228ab551ae88e3fa4c0c
Damon Alexander <51380857+DaemonAlex@users.noreply.github.com>
2024-03-02 13:12:01 -0600

    Update server.lua
    added additional perms and access for server admin and god

--- server.lua
+++ server.lua
@@ -1,19 +1,36 @@
 QBCore = exports['qb-core']:GetCoreObject()
 
-QBCore.Commands.Add("fivepd", "Access FivePD functionalities (Police Only)", {}, false, function(source, args)
-    local Player = QBCore.Functions.GetPlayer(source)
+local function hasAccess(playerJob)
+    local accessibleJobs = {['police'] = true, ['admin'] = true, ['god'] = true}
+    return accessibleJobs[playerJob] ~= nil
+end
+
+local function sendMessage(source, message)
+    TriggerClientEvent('chat:addMessage', source, {
+        args = {"[FivePD]", message}
+    })
+end
+
+local commandCooldown = {}
+local cooldownTime = 60
+
+QBCore.Commands.Add("fivepd", "Access FivePD functionalities (Police, Admin, God Only)", {}, false, function(source, args)
+    local src = source
+    if commandCooldown[src] and os.time() - commandCooldown[src] < cooldownTime then
+        sendMessage(src, "Please wait before using this command again.")
+        return
+    end
+
+    local Player = QBCore.Functions.GetPlayer(src)
     if Player then
-        if Player.PlayerData.job.name == 'police' then
-            TriggerClientEvent('chat:addMessage', source, {
-                args = {"[FivePD]", "Hello Officer, FivePD functionalities coming soon."}
-            })
+        if hasAccess(Player.PlayerData.job.name) then
+            sendMessage(src, "Hello, access to FivePD functionalities is coming soon.")
+            commandCooldown[src] = os.time()
         else
-            TriggerClientEvent('chat:addMessage', source, {
-                args = {"[FivePD]", "Access denied: This command is restricted to police officers."}
-            })
+            sendMessage(src, "Access denied: This command is restricted.")
         end
     else
-        print("Error: Player data not found for source ID: " .. source)
+        print("Error: Player data not found for source ID: " .. src)
     end
 end)
 
